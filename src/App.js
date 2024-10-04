8f09399bcea7805e84d048118bf3910c0333d594
Akash kumar <109230614+i-m-akash@users.noreply.github.com>
2024-10-04 14:27:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './dashboard'; // Your dashboard component
 // import Login from './login'; // Your login component
 import BasicRegistration from './basicregistration';
